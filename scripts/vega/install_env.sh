6b43036aebb420354382bc9235591d8da555360e
48362942+matbun@users.noreply.github.com <matteo.bunino@gmail.com>
2025-03-03 11:40:22 +0100

    refactor scripts

--- scripts/vega/install_env.sh
+++ scripts/vega/install_env.sh
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# shellcheck disable=all
 
 # Load modules
 # NOTE: REFLECT THEM IN THE MAIN README! 
